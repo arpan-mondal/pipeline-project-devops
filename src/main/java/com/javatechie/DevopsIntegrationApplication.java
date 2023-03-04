bb2168fdc4b7de9936c766af14506cf14614193c
ARPAN MONDAL <accesstoarpan@gmail.com>
2023-03-04 18:47:14 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "You have successfully completed the CI/CD Project";
 	}
 
 	public static void main(String[] args) {
